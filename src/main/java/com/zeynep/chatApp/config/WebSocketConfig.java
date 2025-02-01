d9999df8e6404d60c7ec471b4a6b4c8f607745e5
Zeynep ÖZTÜRK <zynpztrk2002@gmail.com>
2025-02-02 01:10:50 +0300

    added websocket configuration.

---
+++ src/main/java/com/zeynep/chatApp/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.zeynep.chatApp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat").setAllowedOrigins("http://localhost:5173").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // set message broker
+        // expect message with /app/send message
+        registry.enableSimpleBroker("/topic"); // topic/chatRoom1 =example channel
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+}
