760c367c3db3c108b7bd13c7ed5d7867ef6d5ab8
Zeynep ÖZTÜRK <zynpztrk2002@gmail.com>
2025-02-02 01:36:26 +0300

    created first model

--- src/main/java/com/zeynep/chatApp/model/ChatMessage.java
+++ src/main/java/com/zeynep/chatApp/model/ChatMessage.java
@@ -1,5 +1,15 @@
 package com.zeynep.chatApp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChatMessage {
 
+    private Long id;
+    private String sender;
+    private String content;
 }
