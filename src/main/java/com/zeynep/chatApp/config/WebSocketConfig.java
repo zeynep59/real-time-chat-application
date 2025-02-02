283b42ab1fed17ac6c748cf1f3dc5d6add9b6607
Zeynep ÖZTÜRK <zynpztrk2002@gmail.com>
2025-02-02 23:53:57 +0300

    created template structure. and added stompjs and  sockjs.

--- src/main/java/com/zeynep/chatApp/config/WebSocketConfig.java
+++ src/main/java/com/zeynep/chatApp/config/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOrigins("http://localhost:5173").withSockJS();
+        registry.addEndpoint("/chat").setAllowedOrigins("http://localhost:8080").withSockJS();
     }
 
     @Override
--- src/main/java/com/zeynep/chatApp/controller/ChatController.java
+++ src/main/java/com/zeynep/chatApp/controller/ChatController.java
@@ -1,13 +1,25 @@
 package com.zeynep.chatApp.controller;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.zeynep.chatApp.model.ChatMessage;
+
 
 @Controller
 public class ChatController {
 
+    //Endpoints
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/message")
     public ChatMessage sendMessage(ChatMessage message){
         return message;
+    }
+
+    @GetMapping("chat")
+    public String chat(){
+        return "chat";
     }
 }
